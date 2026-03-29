c4bb99eb110f8bf9b37fbbb70ecb4e64ad3145ac
rakshitha <rakshitha9870@gmail.com>
2026-03-29 13:39:21 +0530

    Added JUnit test for Website Health Checker

---
+++ src/test/java/com/health/checker/WebsiteCheckerTest.java
@@ -0,0 +1,13 @@
+package com.health.checker;
+
+import org.junit.Test;
+
+public class WebsiteCheckerTest {
+
+    @Test
+    public void testWebsiteChecker() {
+
+        WebsiteChecker.checkWebsite("https://www.google.com");
+
+    }
+}
